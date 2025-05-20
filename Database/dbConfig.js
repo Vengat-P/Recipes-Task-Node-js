cf9a631c0bb8e17e87fdb4f8e169a2ec5772ef35
Vengat-P <cronyvengat33@gmail.com>
2025-05-20 06:55:34 +0530

    Mongo DB Connect Completed

--- Database/dbConfig.js
+++ Database/dbConfig.js
@@ -1,5 +1,21 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 
-
+// initalize dotenv file
 dotenv.config();
+
+//connect to DB 
+
+const connectDB = async()=>{
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB Connected Successfully");
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
+
+
+export default connectDB;
--- index.js
+++ index.js
@@ -1,25 +1,30 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
+import connectDB from './Database/dbConfig.js';
 
-
+// configure dotenv file
 dotenv.config();
 
+//initialize express app
 const app = express();
 
-
+//setup default middleware
 app.use(express.json());
 app.use(cors())
 
+// connect DB by calling connectDB Function
+connectDB();
 
+//default router
 app.length("/",(req,res)=>{
     res.status(200).json({message:"Welcom To Backend"})
 })
 
-
+//port initialization
 const port = process.env.PORT
 
-
+// server starting function
 app.listen(port,()=>{
     console.log("server started");
     
